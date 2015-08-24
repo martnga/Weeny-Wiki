151ae0e3c03783e1d8946d049b4b3ccc3e2f5696
mtnjoroge <mtnjoroge@gmail.com>
2015-08-24 15:58:36 +0300

    Added Article#show to display articles

--- weeny-wiki/app/controllers/articles_controller.rb
+++ weeny-wiki/app/controllers/articles_controller.rb
@@ -1,7 +1,11 @@
 class ArticlesController < ApplicationController
+  before_action :find_article, only: [:show]
+
   def index
   end
 
+  def show
+  end
   def new
     @article = Article.new
   end
@@ -16,7 +20,10 @@ class ArticlesController < ApplicationController
   end
 
   private
+  def find_article
+    @article = Article.find(params[:id])
+  end
   def article_params
-    params.require(article).permit(:title, :content)
+    params.require(:article).permit(:title, :content)
   end
 end
