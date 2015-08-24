77f00f59b5b2022257e157f31ad9404773ba9954
mtnjoroge <mtnjoroge@gmail.com>
2015-08-24 18:25:09 +0300

    Added Categories for Articles

--- weeny-wiki/app/controllers/articles_controller.rb
+++ weeny-wiki/app/controllers/articles_controller.rb
@@ -1,8 +1,14 @@
 class ArticlesController < ApplicationController
   before_action :find_article, only: [:show]
   before_action :authenticate_user!, except: [:index, :show]
+
   def index
+    if params[:category].blank?
     @articles = Article.all.order("created_at DESC")
+    else
+      @category_id = Category.find_by(name: params[:category]).id
+      @articles = Article.where(category_id: @category_id).order("created_at DESC")
+    end
   end
 
   def show
